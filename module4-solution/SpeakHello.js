171e48d77c427b1e5659adee5a6928a9811fd6b9
Victor Bankole <victorbankole11@gmail.com>
2022-06-17 09:23:41 +0100

    module5 assignment

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -2,8 +2,6 @@
 // See Lecture 52, part 2
 
 (function (window) {
-
-
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
@@ -11,7 +9,6 @@ var helloSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
-
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.s
